511e712678f67733be81623a971e1746a20d49d0
ahaoboy <504595380@qq.com>
2025-09-07 18:02:52 +0800

    esm.sh

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import analyzer from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://ahaoboy.github.io/free-piano/",
-  plugins: [react()],
+  base: "https://ahaoboy.github.io/js-engine-benchmark/",
+  plugins: [
+    react(),
+    analyzer({
+      "enabled": !!process.env["analyzer"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      react: "https://esm.sh/react",
+      "react-dom": "https://esm.sh/react-dom",
+      echarts: "https://esm.sh/echarts?standalone",
+      antd: "https://esm.sh/antd?standalone",
+      "free-piano-midi":"https://esm.sh/free-piano-midi?standalone",
+      "fuse.js":"https://esm.sh/fuse.js?standalone",
+      "@ant-design/icons":"https://esm.sh/@ant-design/icons?standalone"
+    },
+  },
 });
