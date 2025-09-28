6f3217c902de9f9b79b0644e15cc300ac5af8be6
ahaoboy <504595380@qq.com>
2025-09-28 11:38:31 +0800

    isDev

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import analyzer from "vite-bundle-analyzer";
 
+const isDev = process.env.NODE_ENV === "development"
+
 // https://vite.dev/config/
 export default defineConfig({
   base: "/free-piano",
@@ -12,14 +14,14 @@ export default defineConfig({
     }),
   ],
   resolve: {
-    alias: {
+    alias: isDev ? {} : {
       react: "https://esm.sh/react",
       "react-dom": "https://esm.sh/react-dom",
       echarts: "https://esm.sh/echarts?standalone",
       antd: "https://esm.sh/antd?standalone",
-      "free-piano-midi":"https://esm.sh/free-piano-midi?standalone",
-      "fuse.js":"https://esm.sh/fuse.js?standalone",
-      "@ant-design/icons":"https://esm.sh/@ant-design/icons?standalone"
+      "free-piano-midi": "https://esm.sh/free-piano-midi?standalone",
+      "fuse.js": "https://esm.sh/fuse.js?standalone",
+      "@ant-design/icons": "https://esm.sh/@ant-design/icons?standalone"
     },
   },
 });
